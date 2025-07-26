bf054c88802ee5c41bd38c1c0dbd12b8ed28dcc4
alienspaces <alienspaces@gmail.com>
2025-07-26 11:55:07 +0000

    Fix default schema path and CI configuration

--- backend/core/config/config.go
+++ backend/core/config/config.go
@@ -22,8 +22,8 @@ type Config struct {
 	// Assets (javascript, css, images, etc.) (default: ./frontend/src/assets)
 	AssetsPath string `env:"ASSETS_PATH" envDefault:"./frontend/src/assets"`
 
-	// Schemas (json) (default: ./backend/schemas)
-	SchemaPath string `env:"SCHEMA_PATH" envDefault:"./backend/schemas"`
+	// Schemas (json) (default: ./backend/schema)
+	SchemaPath string `env:"SCHEMA_PATH" envDefault:"./backend/schema"`
 
 	// Templates (html, email, etc.) (default: ./backend/templates)
 	TemplatesPath string `env:"TEMPLATES_PATH" envDefault:"./backend/templates"`
